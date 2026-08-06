4dcfc07e7d0c26028f1d6e9f1537b4abaa454c25
C:/Program Files/Git/ <thecwme@gmail.com>
2026-08-06 15:49:47 -0500

    Cleaned up evaluation

--- 02_alexnet/evaluate.py
+++ 02_alexnet/evaluate.py
@@ -15,67 +15,7 @@ from tqdm import tqdm
 from .implementation import AlexNet
 from .data import get_dataloader
 
-device = torch.device(torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else 'cpu')
-
-
-def main():
-
-    # Init the model and the optimizer outside the training loop
-
-    learning_rate = 0.01
-    weight_decay = 5e-4
-    momentum = 0.9 
-
-    model = AlexNet().to(device)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(),
-                          lr=learning_rate,
-                          momentum=momentum,
-                          weight_decay=weight_decay
-                        )
-
-    _, trainloader, _, validationloader, _, _ = get_dataloader(batch_size=128,num_workers=6,shuffle=True)
-
-    epochs = 90
-    
-    def train():
-        print("Training started")
-
-        for epoch in range(epochs):
-            running_loss = 0.0
-            average_loss = 0.0
-            model.train()
-
-            loop = tqdm(trainloader, desc=f"Epoch [{epoch+1}/{epochs}]")
-
-            for i, (inputs, labels) in enumerate(loop):
-
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-
-                optimizer.zero_grad()
-                predictions = model(inputs)
-                loss = criterion(predictions, labels)
-                loss.backward()
-                optimizer.step()
-
-                # Update progress bar statistics inline
-                running_loss += loss.item()
-                average_loss = running_loss / len(trainloader)
-
-                stats = {
-                        "loss": average_loss,
-                        "lr": optimizer.param_groups[0]["lr"]
-                    }
-
-                loop.set_postfix(stats)
-
-
-
-
-    print('Finished training')
-
-    # train()
+device = torch.device(torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else 'cpu')   
 
 def validate():
 
