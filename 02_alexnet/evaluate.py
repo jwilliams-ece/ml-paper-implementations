3d63e650cb4f9b9dd0c238479ced1a33228943e6
C:/Program Files/Git/ <thecwme@gmail.com>
2026-08-06 20:47:37 -0500

    Created test prototype

--- 02_alexnet/evaluate.py
+++ 02_alexnet/evaluate.py
@@ -25,7 +25,7 @@ def validate():
 
     running_val_loss = 0.0
 
-    _, _, _, validationloader, _, _ = get_dataloader(batch_size=128, num_workers=6,shuffle=False)
+    _, _, _, validationloader, _, _ = get_dataloader(batch_size=128, num_workers=6,shuffle=False) # Keep False
     criterion = nn.CrossEntropyLoss()
 
     with torch.no_grad():
@@ -43,7 +43,28 @@ def validate():
 
 
 def test():
-    pass    
+    
+    model = AlexNet().to(device)
+    model.load_state_dict(torch.load("model_weights.pth", map_location=device, weights_only=True))
+    model.eval()
+
+    running_val_loss = 0.0
+
+    _, testloader, _, _, _, _ = get_dataloader(batch_size=128, num_workers=6,shuffle=False) # Keep False
+    criterion = nn.CrossEntropyLoss()
+
+    with torch.no_grad():
+        loop = tqdm(testloader)
+        for images, labels in loop:
+            images = images.to(device)
+            labels = labels.to(device)
+            predictions = model(images)
+            loss = criterion(predictions, labels)
+
+            running_val_loss += loss.item()
+
+        total_validation_loss = running_val_loss / len(testloader)
+        print(f"Validation Loss: {total_validation_loss:.4f}")         
         
 if __name__ == "__main__":
     validate()
