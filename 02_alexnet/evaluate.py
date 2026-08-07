58f76c7167571b647c43b3c3591027a1ae0791ff
C:/Program Files/Git/ <thecwme@gmail.com>
2026-08-07 17:30:36 -0500

    Made evaluation functions more modular

--- 02_alexnet/evaluate.py
+++ 02_alexnet/evaluate.py
@@ -15,17 +15,16 @@ from tqdm import tqdm
 from .implementation import AlexNet
 from .data import get_dataloader
 
-device = torch.device(torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else 'cpu')   
 
-def validate():
+def validate(model: nn.Module, device: str, path: str, batch_size: int, num_worlers: int):
 
-    model = AlexNet().to(device)
-    model.load_state_dict(torch.load("model_weights.pth", map_location=device, weights_only=True))
+    model = model.to(device)
+    model.load_state_dict(torch.load(path, map_location=device, weights_only=True))
     model.eval()
 
     running_val_loss = 0.0
 
-    _, _, _, validationloader, _, _ = get_dataloader(batch_size=128, num_workers=6,shuffle=False) # Keep False
+    _, _, _, validationloader, _, _ = get_dataloader(batch_size=batch_size, num_workers=num_worlers,shuffle=False) # Keep False
     criterion = nn.CrossEntropyLoss()
 
     correct = 0.0
@@ -51,14 +50,14 @@ def validate():
 
         accuracy = correct / total * 100
         total_validation_loss = running_val_loss / len(validationloader)
-        
+
         print(f"Validation Loss: {total_validation_loss:.4f}")   
         print(f"Accuracy: {accuracy:.2f}%")   
 
 
-def test():
+def test(model: nn.Module, device: str, path: str, batch_size: int, num_worlers: int):
     
-    model = AlexNet().to(device)
+    model = model.to(device)
     model.load_state_dict(torch.load("model_weights.pth", map_location=device, weights_only=True))
     model.eval()
 
@@ -66,6 +65,9 @@ def test():
 
     _, testloader, _, _, _, _ = get_dataloader(batch_size=128, num_workers=6,shuffle=False) # Keep False
     criterion = nn.CrossEntropyLoss()
+
+    correct = 0.0
+    total = 0
 
     with torch.no_grad():
         loop = tqdm(testloader)
@@ -75,10 +77,19 @@ def test():
             predictions = model(images)
             loss = criterion(predictions, labels)
 
+            # Accuracy tracking
+            for prediction, label in zip(predictions,labels):
+                predicted_class = torch.argmax(prediction)
+                total += 1
+                if predicted_class == label:
+                    correct += 1
+
             running_val_loss += loss.item()
 
+        accuracy = correct / total * 100
         total_validation_loss = running_val_loss / len(testloader)
         print(f"Validation Loss: {total_validation_loss:.4f}")         
+        print(f"Accuracy: {accuracy:.2f}%")         
         
 if __name__ == "__main__":
     validate()
