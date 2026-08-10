20383eaf611a6a238e4bfa09747fefcdbcd25c4d
C:/Program Files/Git/ <thecwme@gmail.com>
2026-08-10 10:58:04 -0500

    Updated evaluation loop to average 10 images

--- 02_alexnet/evaluate.py
+++ 02_alexnet/evaluate.py
@@ -11,59 +11,92 @@ import torch
 import torch.nn as nn
 from torch import optim
 from tqdm import tqdm
+from torchmetrics.classification import MulticlassF1Score
 
 from .implementation import AlexNet
 from .data import get_dataloader
 
 
-def validate(model: nn.Module, device: str, path: str, batch_size: int, num_worlers: int):
+def validate(model: nn.Module, device: str, path: str, batch_size: int, num_workers: int):
 
+    model = model.to(device)
+    model.load_state_dict(torch.load(path, map_location=device, weights_only=True))
+    model.eval()
+
+    num_classes = 100
+
+    running_val_loss = 0.0
+    correct = 0.0
+    total = 0.0
+
+    _, _, _, validationloader, _, _ = get_dataloader(batch_size=batch_size, num_workers=num_workers,shuffle=False) # Keep False
+    criterion = nn.CrossEntropyLoss()
+
+    f1_metrics = MulticlassF1Score(
+        num_classes=num_classes,
+        average=None,
+    ).to(device)
+
+    with torch.no_grad():
+        loop = tqdm(validationloader)
+        for image_crops, labels in loop:
+
+            # crops: 10 tensors, each [B,C,224,224]
+            image_crops = torch.stack(image_crops,dim=1)
+
+            # [B,10,C,H,W]
+            B,N,C,H,W = image_crops.shape
+
+            # move data to GPU
+            image_crops = image_crops.to(device)
+            labels = labels.to(device)
+
+            # treat all crops as idividual images
+            image_crops = image_crops.view(B * N, C, H, W)
+
+            # [B*10, num_classes]
+            logits = model(image_crops)
+
+            # [B,N, numclasses]
+            logits = logits.view(B,N,-1)
+
+            # compute average logits
+            avg_logits = logits.mean(dim=1)
+            
+            # compute loss
+            loss = criterion(avg_logits, labels)
+
+            # Accuracy logging
+            predicted_classes = avg_logits.argmax(dim=1)
+            correct += (predicted_classes == labels).sum().item()
+            total += B
+
+            # weighted loss
+            running_val_loss += loss.item() * B
+
+            # F1 Score tracking
+            f1_metrics.update(avg_logits, labels)
+            
+
+    accuracy = correct / total * 100
+    total_validation_loss = running_val_loss / total
+    scores = f1_metrics.compute()
+
+    print(f"Validation Loss: {total_validation_loss:.4f}")   
+    print(f"Accuracy: {accuracy:.2f}%")   
+    for class_idx, score in enumerate(scores):
+        print(f"Class {class_idx}: F1 = {score:.4f}")
+
+
+def test(model: nn.Module, device: str, path: str, batch_size: int, num_workers: int):
+    
     model = model.to(device)
     model.load_state_dict(torch.load(path, map_location=device, weights_only=True))
     model.eval()
 
     running_val_loss = 0.0
 
-    _, _, _, validationloader, _, _ = get_dataloader(batch_size=batch_size, num_workers=num_worlers,shuffle=False) # Keep False
-    criterion = nn.CrossEntropyLoss()
-
-    correct = 0.0
-    total = 0.0
-
-    with torch.no_grad():
-        loop = tqdm(validationloader)
-        for images, labels in loop:
-            images = images.to(device)
-            labels = labels.to(device)
-            predictions = model(images)
-            loss = criterion(predictions, labels)
-
-
-            # Accuracy tracking
-            for prediction, label in zip(predictions,labels):
-                predicted_class = torch.argmax(prediction)
-                total += 1
-                if predicted_class == label:
-                    correct += 1
-
-            running_val_loss += loss.item()
-
-        accuracy = correct / total * 100
-        total_validation_loss = running_val_loss / len(validationloader)
-
-        print(f"Validation Loss: {total_validation_loss:.4f}")   
-        print(f"Accuracy: {accuracy:.2f}%")   
-
-
-def test(model: nn.Module, device: str, path: str, batch_size: int, num_worlers: int):
-    
-    model = model.to(device)
-    model.load_state_dict(torch.load("model_weights.pth", map_location=device, weights_only=True))
-    model.eval()
-
-    running_val_loss = 0.0
-
-    _, testloader, _, _, _, _ = get_dataloader(batch_size=128, num_workers=6,shuffle=False) # Keep False
+    _, testloader, _, _, _, _ = get_dataloader(batch_size=batch_size, num_workers=num_workers,shuffle=False) # Keep False
     criterion = nn.CrossEntropyLoss()
 
     correct = 0.0
@@ -71,10 +104,10 @@ def test(model: nn.Module, device: str, path: str, batch_size: int, num_worlers:
 
     with torch.no_grad():
         loop = tqdm(testloader)
-        for images, labels in loop:
-            images = images.to(device)
+        for image_crops, labels in loop:
+            image_crops = image_crops.to(device)
             labels = labels.to(device)
-            predictions = model(images)
+            predictions = model(image_crops)
             loss = criterion(predictions, labels)
 
             # Accuracy tracking
