3b6fda925ddbe89dbf1793aae81c58520c6e77f6
/ <thecwme@gmail.com>
2026-08-13 22:47:00 -0500

    function implementations for gradient descent and backprop

--- 07_word2vec/train.py
+++ 07_word2vec/train.py
@@ -12,18 +12,54 @@ model = CBOW(vocab_size=vocab_size, dimension_size=EMBEDDING_DIM)
 data_pairs, _ , _ = get_data()
 
 def softmax(logits):
-    output = []
-    probs = []
+    shifted_logits = logits - np.max(logits)
 
-    for i in logits:
-        for j in logits:
-            x += np.exp(j)
-        
-        prob = np.exp(i) / x
+    exp_values = np.exp(shifted_logits)
+    denominator = exp_values.sum()
 
-    return output
+    probabilities = exp_values / denominator
+
+    return probabilities
+
+def negative_log_loss(probabilites, target):
+    loss = - (np.log(probabilites[target] + 1e-10))
+    return loss
+
+def do_gradient(predictions, target):
+    arr = predictions.copy()
+    arr[target] -= 1
+    do = arr
+
+    return do
+
+def embeddings_gradient(h, output_grad):
+    dW = np.outer(np.transpose(h), output_grad)
+
+    return dW
+
+def dh_gradient(output_grad):
+    dh = output_grad @ np.transpose(model.output_matrix)
+
+    return dh
+
+def dE_gradient(context,dh):
+    c = len(context)
+    dE_context = dh/c
+
+    return dE_context
+
+def update_weights(lr, context, dE_context):
+    for vector in context:
+        model.embbeddings_matrix[vector] = (model.embbeddings_matrix[vector]) -  (lr * dE_context)
+
+
+
 
 def train():
 
     for context, target in data_pairs:
         logits = model(context)
+
+
+
+
